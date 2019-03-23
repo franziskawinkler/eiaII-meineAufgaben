aab1a5a4c7b859a492303cbf814a2009508f424e
franziskawinkler <44029065+franziskawinkler@users.noreply.github.com>
2019-03-23 14:25:03 +0100

    interactive_website_done

---
+++ Aufgabe 0/js/main.js
@@ -0,0 +1,9 @@
+/*Funktion für prompt und greeter*/
+function myFunction() {
+    var person = prompt("What´s your name?", "Tony Stark");
+    if (person != null) {
+        document.getElementById("demo").innerHTML =
+            "¡Hola" + " " + person + "! What´s up?" + " " + "It´s all about the magic! :P";
+    }
+}
+//# sourceMappingURL=main.js.map
