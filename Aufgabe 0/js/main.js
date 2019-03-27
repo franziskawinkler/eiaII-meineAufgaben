ebc28105cf3bcd35e502a892d79feff9ed304659
franziskawinkler <44029065+franziskawinkler@users.noreply.github.com>
2019-03-27 21:14:07 +0100

    done

--- Aufgabe 0/js/main.js
+++ Aufgabe 0/js/main.js
@@ -3,7 +3,7 @@ function nameEingeben() {
     /*Variable vom Typ string; prompt-Box mit Namenseingabe*/
     let _nameDerPerson = prompt("What´s your name?", "Tony Stark");
     /*Begrüßung der Person auf meiner Seite*/
-    let _herzlichWillkommen = "Hallo," + "" + _nameDerPerson + ", herzlich Willkommen auf dieser Seite!";
+    let _herzlichWillkommen = "Hallo" + " " + _nameDerPerson + ", herzlich Willkommen auf dieser Seite!";
     /*Ausgabe auch auf der Konsole*/
     console.log(_herzlichWillkommen);
     document.getElementById("greeter").innerHTML = _herzlichWillkommen;
