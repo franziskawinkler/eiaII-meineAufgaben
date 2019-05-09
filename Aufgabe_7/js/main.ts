07abf7fb664ac4d61e97bdf3e0f4a002e30cb2ab
franziskawinkler <44029065+franziskawinkler@users.noreply.github.com>
2019-05-09 12:45:23 +0200

    datum_angepasst

--- Aufgabe_7/js/main.ts
+++ Aufgabe_7/js/main.ts
@@ -2,7 +2,7 @@
 Aufgabe: <7>
 Name: <Franziska Winkler>
 Matrikel: <260944>
-Datum: <07.05.2019>
+Datum: <09.05.2019>
 	
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
