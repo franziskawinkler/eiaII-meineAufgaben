855a2f4963ea143c8719976f9d7e34799063d4ef
franziskawinkler <44029065+franziskawinkler@users.noreply.github.com>
2019-07-26 12:38:20 +0200

    newtry

--- aufgabe_14/customer/server/server.ts
+++ aufgabe_14/customer/server/server.ts
@@ -1,6 +1,6 @@
 import * as Http from "http"; 
 import * as Url from "url";
-import * as Database from "./database";
+//import * as Database from "./database";
 namespace Server { 
 	console.log("Starting server"); 
 	let port: number = Number(process.env.PORT); 
@@ -9,7 +9,7 @@ namespace Server {
 
 	let server: Http.Server = Http.createServer(); 
 	server.addListener("request", handleRequest);
-	server.addListener("request", handleAdminRequest);
+	//server.addListener("request", handleAdminRequest);
 	server.addListener("listening", handleListen); 
 	server.listen(port); 
 
@@ -32,42 +32,42 @@ namespace Server {
 		_response.end(); //schließt response ab und signalisert dem server, vollständig zu sein
 	} //die Funktion wird geschlossen
 
-	function handleAdminRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-		console.log("Request received");
+	//function handleAdminRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+	//	console.log("Request received");
 		
-		let query: AssocStringString = <AssocStringString>Url.parse(_request.url, true).query;
-		let command: string = query["command"];
+	//	let query: AssocStringString = <AssocStringString>Url.parse(_request.url, true).query;
+	//	let command: string = query["command"];
 	
-		switch (command) {
-			case "insert":
-				let eisdealer: EISDEALER = {
-					eissorten: query["name"],
-					toppings: query["topping"],
-					saucen: query["sauce"]
-				};
+	//	switch (command) {
+	//		case "insert":
+	//			let eisdealer: EISDEALER = {
+	//				eissorten: query["name"],
+	//				toppings: query["topping"],
+	//				saucen: query["sauce"]
+	//			};
 			   
-				Database.insert(eisdealer);
-				respond(_response, "storing data");
-				break;
-			case "refresh":
-				Database.findAll(findCallback);
-				break;
-			default:
-				respond(_response, "unknown command: " + command);
-				break;
-		}
+	//			Database.insert(eisdealer);
+	//			respond(_response, "storing data");
+	//			break;
+	//		case "refresh":
+	//			Database.findAll(findCallback);
+	//			break;
+	//		default:
+	//			respond(_response, "unknown command: " + command);
+	//			break;
+	//	}
 	
 		// findCallback is an inner function so that _response is in scope
-		function findCallback(json: string): void {
-			respond(_response, json);
-		}
+	//	function findCallback(json: string): void {
+	//		respond(_response, json);
+	//	}
 	}
 	
-	function respond(_response: Http.ServerResponse, _text: string): void {
-		//console.log("Preparing response: " + _text);
-		_response.setHeader("Access-Control-Allow-Origin", "*");
-		_response.setHeader("content-type", "text/html; charset=utf-8");
-		_response.write(_text);
-		_response.end();
-	}
-} 
+	//function respond(_response: Http.ServerResponse, _text: string): void {
+	//	//console.log("Preparing response: " + _text);
+	//	_response.setHeader("Access-Control-Allow-Origin", "*");
+	//	_response.setHeader("content-type", "text/html; charset=utf-8");
+	//	_response.write(_text);
+	//	_response.end();
+	//}
+//} 
