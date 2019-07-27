49d0c4d1010e8ea70ea6d421464f2828d7e4bc65
franziskawinkler <44029065+franziskawinkler@users.noreply.github.com>
2019-07-27 17:02:56 +0200

    server

--- aufgabe_14/server/server.ts
+++ aufgabe_14/server/server.ts
@@ -51,13 +51,14 @@ namespace Eisdealer {
 
 			_response.setHeader("content-type", "text/html; charset=utf-8"); 
 			_response.setHeader("Access-Control-Allow-Origin", "*"); 
-			_response.write("<h4>Folgendes ist auf dem Server eingegangen:</h4>");
+			_response.write("");
 			let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
 			for (let key in url.query)
 				_response.write("<p>" + key + url.query[key] + "</p>");
 			console.log(_request.url); 
 			_response.end(); 
 		} 
+		
 	}
 	}
 
