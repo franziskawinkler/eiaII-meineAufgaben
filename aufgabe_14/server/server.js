2223167ec272c476fdda4ff33a6aaa3d4156a4e7
franziskawinkler <44029065+franziskawinkler@users.noreply.github.com>
2019-07-27 11:41:32 +0200

    server

--- aufgabe_14/server/server.js
+++ aufgabe_14/server/server.js
@@ -45,7 +45,7 @@ var Server;
             console.log("I hear voices!");
             _response.setHeader("content-type", "text/html; charset=utf-8");
             _response.setHeader("Access-Control-Allow-Origin", "*");
-            _response.write("<h4>folgende Bestellung ist auf dem Server eingegangen:</h4>");
+            _response.write("<h4>Folgendes ist auf dem Server eingegangen:</h4>");
             let url = Url.parse(_request.url, true);
             for (let key in url.query)
                 _response.write("<p>" + key + url.query[key] + "</p>");
